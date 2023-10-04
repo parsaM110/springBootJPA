8888a69918c3180c5376ed3e2eac90c812db3241
parsaM110 <pmomeni80@gmail.com>
2023-10-04 14:29:06 +0330

    'make a studentRepository method _> findStudentByEmail :20'

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -30,35 +30,10 @@ public class Application {
             System.out.println("\u001B[33m" + "Adding maria and Ahmad" + "\u001B[0m");
             studentRepository.saveAll(List.of(maria, ahmed));
 
-            System.out.println("\u001B[33m" + "Number of students: " + "\u001B[0m");
-            System.out.println("\u001B[34m" + studentRepository.count()+ "\u001B[0m");
-
-            studentRepository.findById(2L).ifPresentOrElse(
-//                    student -> {
-//                        System.out.println(student);
-//                    }
-                    //todo : instead of lambda we use expression lambda :|
-                    System.out::println
-                    , () -> System.out.println("Student with ID 2 not found")
-//                    {
-//                System.out.println("Student with ID 2 not found");
-//            }
-                    //todo : instead of {} we use only the expression but without ; at end
-            );
-
-            studentRepository.findById(3L).ifPresentOrElse(
-                    System.out::println
-                    , () -> System.out.println("Student with ID 3 not found"));
-
-            System.out.println("\u001B[33m" + "select all student: " + "\u001B[0m");
-            List<Student> students = studentRepository.findAll();
-            students.forEach(System.out::println);
-
-            System.out.println("\u001B[33m" + "delete maria  " + "\u001B[0m");
-            studentRepository.deleteById(1L);
-
-            System.out.println("\u001B[33m" + "Number of students  " + "\u001B[0m");
-            System.out.println( "\u001B[34m" + studentRepository.count()+ "\u001B[0m");
+            studentRepository.findStudentByEmail("ahmed.ali@amigoscode.edu")
+                    .ifPresentOrElse(
+                            System.out::println,
+                            () -> System.out.println("Student with email ahmed.ali@amigoscode.edu not found"));
 
         };
     }
