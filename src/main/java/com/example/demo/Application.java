c478f4cb99441fd85c6379ee75d1bf44e00608d2
parsaM110 <pmomeni80@gmail.com>
2023-10-04 14:11:57 +0330

    'adding some jpa functionality like delete and ... and println it colorful/ make update -> create&drop for each time the dataBase got update the previous data deleted :18 & 19'

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Application {
 
@@ -19,7 +21,45 @@ public class Application {
                     "Jones",
                     "maria.jones@amigoscode.edu",
                     21);
-            studentRepository.save(maria);
+
+            Student ahmed = new Student("Ahmed",
+                    "Ali",
+                    "ahmed.ali@amigoscode.edu",
+                    18);
+
+            System.out.println("\u001B[33m" + "Adding maria and Ahmad" + "\u001B[0m");
+            studentRepository.saveAll(List.of(maria, ahmed));
+
+            System.out.println("\u001B[33m" + "Number of students: " + "\u001B[0m");
+            System.out.println("\u001B[34m" + studentRepository.count()+ "\u001B[0m");
+
+            studentRepository.findById(2L).ifPresentOrElse(
+//                    student -> {
+//                        System.out.println(student);
+//                    }
+                    //todo : instead of lambda we use expression lambda :|
+                    System.out::println
+                    , () -> System.out.println("Student with ID 2 not found")
+//                    {
+//                System.out.println("Student with ID 2 not found");
+//            }
+                    //todo : instead of {} we use only the expression but without ; at end
+            );
+
+            studentRepository.findById(3L).ifPresentOrElse(
+                    System.out::println
+                    , () -> System.out.println("Student with ID 3 not found"));
+
+            System.out.println("\u001B[33m" + "select all student: " + "\u001B[0m");
+            List<Student> students = studentRepository.findAll();
+            students.forEach(System.out::println);
+
+            System.out.println("\u001B[33m" + "delete maria  " + "\u001B[0m");
+            studentRepository.deleteById(1L);
+
+            System.out.println("\u001B[33m" + "Number of students  " + "\u001B[0m");
+            System.out.println( "\u001B[34m" + studentRepository.count()+ "\u001B[0m");
+
         };
     }
 
