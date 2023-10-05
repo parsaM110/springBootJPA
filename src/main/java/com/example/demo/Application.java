d3bd908f7a5c8fa188da149ffcf79e2c1ae5c1fb
parsaM110 <pmomeni80@gmail.com>
2023-10-05 10:52:29 +0330

    'paging in PagingAndSortingRepository  :30&31'

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 
 import java.util.List;
@@ -18,13 +20,23 @@ public class Application {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-//            Sort sort = Sort.by(Sort.Direction.DESC, "firstName");
-            Sort sort = Sort.by( "firstName").ascending().and(Sort.by("age").descending());
-            studentRepository.findAll(sort)
-                    .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
+            //sorting(studentRepository);
+
+            PageRequest pageRequest = PageRequest.of(
+                    0,
+                    5,
+                    Sort.by("firstName").ascending());
+            Page<Student> page = studentRepository.findAll(pageRequest);
+            System.out.println(page);
 
         };
 
+    }
+
+    private static void sorting(StudentRepository studentRepository) {
+        Sort sort = Sort.by( "firstName").ascending().and(Sort.by("age").descending());
+        studentRepository.findAll(sort)
+                .forEach(student -> System.out.println(student.getFirstName() + " " + student.getAge()));
     }
 
 }
