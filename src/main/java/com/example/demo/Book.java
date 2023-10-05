28d753f61fd2a6c7afb55f50205e6feb99620166
parsaM110 <pmomeni80@gmail.com>
2023-10-05 18:35:32 +0330

    'adding the book entity with Many2one relationship :44'

---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,54 @@
+package com.example.demo;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity(name = "Book")
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @SequenceGenerator(
+            name = "book_sequence",
+            sequenceName = "book_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "book_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "book_name",
+            nullable = false,
+            columnDefinition = "TEXT"
+    )
+    private String bookName;
+
+    @Column(
+            name = "created_at",
+            nullable = false,
+            columnDefinition = "TIMESTAMP WITHOUT TIME ZONE"
+    )
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(
+            name = "student_id",
+            nullable = false,
+            referencedColumnName = "id",
+            foreignKey = @ForeignKey(
+                    name="student_book_fk"
+            )
+    )
+    private Student student;
+}
